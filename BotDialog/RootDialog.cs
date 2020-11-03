7f125db4462f01cca2f05e5b4b35fbd80cf8e29a
poojithachityala <poojithareddy1031@gmail.com>
2020-11-03 13:11:08 +0530

    added files

---
+++ BotDialog/RootDialog.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Actions;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Conditions;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Recognizers;
+
+namespace AdaptiveCard1.BotDialog
+{
+    public class RootDialog : ComponentDialog
+    {
+        public RootDialog() : base(nameof(RootDialog))
+        {
+           // var templates = Templates.ParseFile(Path.Combine(".", "Dialogs", "RootDialog.lg"));
+
+            var rootDialog = new AdaptiveDialog
+            {
+                Recognizer = CreateRecognizer(),
+                Triggers = new List<OnCondition>
+                {
+                    new OnIntent()
+                    {
+                        Intent = "hi",
+                        Actions = new List<Dialog>() {new SendActivity("how can i help you")}
+                    },
+
+                    new OnIntent()
+                    {
+                        Intent="hello",
+                        Actions = new List<Dialog>() {new SendActivity("How can i help you")}
+                    },
+                    new OnIntent()
+                    {
+                        Intent="issue",
+                        Actions=new List<Dialog>(){new SendActivity("what is the issue you are facing ")}
+                        
+                    },
+                    new OnIntent()
+                    {
+                        Intent="incident",
+                        Actions=new List<Dialog>(){new SendActivity("Can i create incident for you ")}
+
+                    },
+                    new OnUnknownIntent()
+                    {
+                        Actions=new List<Dialog>(){new SendActivity("sorry,i don't undersatnd")}
+                    }
+
+
+                }
+            };
+
+            AddDialog(rootDialog);
+
+            InitialDialogId = nameof(AdaptiveDialog);
+        }
+
+
+        private Recognizer CreateRecognizer()
+        {
+            var reg = new RegexRecognizer
+            {
+                Intents = new List<IntentPattern>
+                {
+                    new IntentPattern("hi", "(?i)hi"),
+                    new IntentPattern("hello", "(?i)hello"),
+                    new IntentPattern("issue","(?i)issue"),
+                    new IntentPattern("incident","(?i)incident")
+                }
+            };
+            return reg;
+        }
+    }
+}
---
+++ Helper/DialogBot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+
+namespace AdaptiveCard1.Helper
+{
+    public class DialogBot<T> : ActivityHandler where T: Dialog
+    {
+        protected readonly BotState ConversationBotState;
+        protected readonly BotState UserBotState;
+        private readonly Dialog _dialog;
+
+        public DialogBot(ConversationState conversationState,UserState userState,T dialog)
+        {
+            ConversationBotState = conversationState;
+            UserBotState = userState;
+            _dialog = dialog;
+        }
+
+
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = new CancellationToken())
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+
+            await ConversationBotState.SaveChangesAsync(turnContext,false, cancellationToken);
+            await UserBotState.SaveChangesAsync(turnContext, false, cancellationToken);
+
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await _dialog.RunAsync(turnContext, ConversationBotState.CreateProperty<DialogState>(nameof(DialogState)),
+                cancellationToken);
+        }
+       
+
+
+    }
+}
